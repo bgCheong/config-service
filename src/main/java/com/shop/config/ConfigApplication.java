9f6a43c494a643c3040935a5c933eb1884f29eec
jbg4354 <jbg4354@naver.com>
2025-07-24 17:43:25 +0900

    first commit

--- src/main/java/com/shop/config/ConfigApplication.java
+++ src/main/java/com/shop/config/ConfigApplication.java
@@ -1,6 +1,8 @@
 package com.shop.config;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
